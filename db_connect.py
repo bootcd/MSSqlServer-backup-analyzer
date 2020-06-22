7178207e3e95e6a97c3846200fc2583697ca623f
mymac <ultra.bootcd@gmail.com>
2020-06-22 13:45:40 +0300

    little fixing

--- db_connect.py
+++ db_connect.py
@@ -1,19 +1,22 @@
 import pyodbc
 
 # vars
+drivers = [item for item in pyodbc.drivers()]
+driver = drivers[-1]
+print("driver:{}".format(driver))
 
 db_name = "msdb"
 
 
-server_name = "116.202.79.250"
-username = "sa"
-password = "2-uFaVcV239869"
+server_name = ""
+username = ""
+password = ""
 
 
 # function returns "connection" object for MS SQL Server connect
 
 def db_connect(server, database_name):
     connection = pyodbc.connect(
-        'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database_name + ';' + 'UID=' + username + ';PWD=' + password + ';')
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database_name + ';' + 'UID=' + username + ';PWD=' + password + ';')
 
     return connection
